f9648f4b108ed9dac9324f9862dc4f14f1964034
Jun Jo <j.jo@kist-europe.de>
2017-09-26 16:16:16 +0200

    motor fault classifier v0.1: cnn.py - reconstruct the structure of codes - implement training phase & test phase

--- cnn.py
+++ cnn.py
@@ -9,152 +9,318 @@ import tensorflow as tf
 
 print('Tensorflow version ' + tf.__version__)
 
-TOTAL_STEP = 400
-INPUT_DIR = '/mnt/data/camus/images/20170919/'
-MODEL_DIR = './tmp/model/'
-LOG_DIR = './tmp/log/'
+class DataGen(object):
+  '''
+  Generate input data batches from png images
+  args
+    file_path  = input image(png) file path
+    batch_size = batch size for each training step
+    one_hot    = True: output labels (Y_) as one_hot arrays
+    train      = modify return format to train / test phase
+  return
+    if train = True:
+      XY_s = [([X batch], [Y_ batch]), ..., ([X batch], [Y_ batch])]
+    else:
+      XY_s = ([X, ..., X], [Y_, ..., Y_], [img_path, ..., img_path])
+  '''
+  def __init__(self, file_path, batch_size=1, one_hot=True, train=True):
+    self.XY_s = []
+    files = os.listdir(file_path)
+    files = [os.path.join(file_path, s) for s in files]
+    file_q = tf.train.string_input_producer(files, shuffle=train)
+    reader = tf.WholeFileReader()
+    path, data = reader.read(file_q)
+    img = tf.image.decode_png(data, channels=3)
+    with tf.Session() as sess:
+      tf.global_variables_initializer().run()
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+      XY_tuple = ([], []) if train else ([], [], [])
+      for i in range(len(files)):
+        X, Y_ = sess.run([img, path])
+        XY_tuple[0].append(X)
+        if not train: XY_tuple[2].append(Y_)
+        Y_value = int(Y_.strip('.png').split('-')[3])
+        if one_hot:
+          XY_tuple[1].append(tf.one_hot(Y_value,4).eval())
+        else:
+          XY_tuple[1].append(Y_value)
+      coord.request_stop()
+      coord.join(threads)
+      if train:
+        for X, Y_ in self._chunks(XY_tuple, batch_size):
+          self.XY_s.append((X, Y_))
+      else:
+        self.XY_s = XY_tuple
 
-# input X: 1000 x 1000 rgb color image
-X = tf.placeholder(tf.float32, [None, 1000, 1000, 3])
-# target values Y_: 0=normal, 1=stator fault, 2=rotor fault, 3=bearing fault
-Y_ = tf.placeholder(tf.float32, [None, 4])
-# learning rate
-global_step = tf.Variable(0, name='global_step', trainable=False) 
-lr = tf.train.exponential_decay(0.001, global_step, int(TOTAL_STEP/5), 0.5, staircase=True)
-#lr = tf.placeholder(tf.float32)
-# dropout keep probability
-p_keep = tf.placeholder(tf.float32)
+  def out(self):
+    return self.XY_s
 
-phase_train = tf.placeholder(tf.bool)
+  # devide lists in XY_tuple into (length n) lists (batch)
+  @staticmethod
+  def _chunks(XY_tuple, n):
+    for i in range(0, len(XY_tuple[0]), n):
+      yield XY_tuple[0][i:i+n], XY_tuple[1][i:i+n]
 
-def batch_norm(x, n_out, phase_train):
-  with tf.variable_scope('bn'):
-    beta = tf.Variable(tf.constant(0.0, shape=[n_out]), name='beta', trainable=True)
-    gamma = tf.Variable(tf.constant(1.0, shape=[n_out]), name='gamma', trainable=True)
-    batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
+
+class ConvLayer(object):
+  '''
+  Construct a convolutional 2D layer and its summaries
+  args:
+    image  = input image array
+    ch_in  = input image channel size (e.g. rgb = 3)
+    ch_out = output channel size (number of kernels)
+    size   = size of kernel (patch)
+    stride = kernel (patch) stride
+  '''
+  def __init__(self, image, ch_in, ch_out, size, stride):
+    self.img = image
+    self.strd = stride
+    _W_shape = [size, size, ch_in, ch_out]
+    self.W = tf.Variable(tf.truncated_normal(_W_shape, stddev=0.1), trainable=True, name='W')
+    self._summary(self.W, 'W')
+    self.B = tf.Variable(tf.constant(0.1, tf.float32, [ch_out]), trainable=True, name='B')
+    self._summary(self.B, 'B')
+
+  def out(self):
+    return tf.nn.relu(
+        tf.nn.conv2d(self.img, self.W, strides=[1, self.strd, self.strd, 1], padding='SAME') + self.B)
+
+  @staticmethod
+  def _summary(var, name):
+    _mean = tf.reduce_mean(var)
+    _variance = tf.reduce_mean(tf.square(var - _mean))
+    tf.summary.scalar(name+'_mean', _mean)
+    tf.summary.scalar(name+'_variance', _variance)
+    tf.summary.histogram(name, var)
+
+
+class FCLayer(object):
+  '''
+  Construct a fully connected layer and its summaries
+  args:
+    input_ = input array
+    n_in   = input size
+    n_out  = output size
+    relu   = pass through ReLU as activation function (True or False)
+  '''
+  def __init__(self, input_, n_in, n_out, relu=False):
+    self.input_ = input_
+    self.n_in = n_in
+    self.n_out = n_out
+    self.relu = relu
+    self.W = tf.Variable(tf.truncated_normal([n_in, n_out], stddev=0.1), trainable=True, name='W')
+    self._summary(self.W, 'W')
+    self.B = tf.Variable(tf.constant(0.0, tf.float32, [n_out]), trainable=True, name='B')
+    self._summary(self.B, 'B')
+
+  def out(self):
+    if self.relu:
+      return tf.nn.relu(tf.matmul(self.input_, self.W) + self.B)
+    else:
+      return tf.matmul(self.input_, self.W) + self.B
+
+  @staticmethod
+  def _summary(var, name):
+    _mean = tf.reduce_mean(var)
+    _variance = tf.reduce_mean(tf.square(var - _mean))
+    tf.summary.scalar(name+'_mean', _mean)
+    tf.summary.scalar(name+'_variance', _variance)
+    tf.summary.histogram(name, var)
+
+
+class BatchNorm(object):
+  '''
+  Construct a batch normalization for input array
+  args
+    input_ = input array
+    n_out  = output size
+    train  = True: train phase, False: test phase
+  '''
+  def __init__(self, input_, n_out, train):
+    self.input_ = input_
+    self.beta = tf.Variable(tf.constant(0.0, shape=[n_out]), name='beta', trainable=True)
+    self.gamma = tf.Variable(tf.constant(1.0, shape=[n_out]), name='gamma', trainable=True)
+    batch_mean, batch_var = tf.nn.moments(input_, [0,1,2], name='moments')
     ema = tf.train.ExponentialMovingAverage(decay=0.5)
-
     def mean_var_with_update():
       ema_apply_op = ema.apply([batch_mean, batch_var])
       with tf.control_dependencies([ema_apply_op]):
         return tf.identity(batch_mean), tf.identity(batch_var)
-    mean, var = tf.cond(phase_train, mean_var_with_update,
+    self.mean, self.var = tf.cond(train, mean_var_with_update,
                         lambda: (ema.average(batch_mean), ema.average(batch_var)))
-    return tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
 
-# devide list l into (length n) lists
-def chunks(l, n):
-  for i in range(0, len(l[0]), n):
-    yield l[0][i:i+n], l[1][i:i+n]
+  def out(self):
+    return tf.nn.batch_normalization(self.input_, self.mean, self.var, self.beta, self.gamma, 1e-3)
 
-def summaries(var, name):
-  with tf.name_scope('summaries'):
-    mean = tf.reduce_mean(var)
-    tf.summary.scalar('mean/' + name, mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-    tf.summary.scalar('stddev/' + name, stddev)
-    tf.summary.scalar('max/' + name, tf.reduce_max(var))
-    tf.summary.scalar('min/' + name, tf.reduce_min(var))
-    tf.summary.histogram(name, var)
 
-K = 25    # first conv. output depth
-L = 50    # send conv. output depth
-M = 25    # third conv.
-N = 100   # fully connected layer
+def model(X, Y_, train=True):
+  '''
+  Define a DNN inference model
+  args:
+    X     = input image array
+    Y_    = labels of input (solutions of Y)
+    train = True: train phase, False: test phase
+  returns:
+    Y     = predicted output array (e.g. [1, 0, 0, 0])
+    cross_entropy
+    accuracy
+    incorrects = indices of incorrect inference
 
-W1 = tf.Variable(tf.truncated_normal([100, 100, 3, K], stddev=0.1), name='W1')
-summaries(W1, 'W1')
-B1 = tf.Variable(tf.constant(0.1, tf.float32, [K]), name='B1')
-summaries(B1, 'B1')
-W2 = tf.Variable(tf.truncated_normal([50, 50, K, L], stddev=0.1), name='W2')
-B2 = tf.Variable(tf.constant(0.1, tf.float32, [L]), name='B2')
-W3 = tf.Variable(tf.truncated_normal([25, 25, L, M], stddev=0.1), name='W3')
-B3 = tf.Variable(tf.constant(0.1, tf.float32, [M]), name='B3')
+  X:      [1000 x 1000 x 3] HWC image volume
+  Conv1 : [100 x 100 x K1] output volume after [100 x 100 x 3] kernel with stride 10
+  Conv2 : [50 x 50 x K2] output volume after [10 x 10 x K1] kernel with stride 2
+  Conv3 : [25 x 25 x K3] output volume after [5 x 5 x K2] kernel with stride 2
+  Conv4 : [25 x 25 x K4] output volume after [3 x 3 x K3] kernel with stride 1
+  Full1 : [F1] output nodes from [25 x 25 x K4] input nodes
+  Full2 : [F2] output nodes from [F1] input nodes
+  Output: [4] ouput nodes from [F2] input nodes
+  '''
+  K1 = 10    # first conv. kernel depth
+  K2 = 20    # second conv. kernel depth
+  K3 = 40    # third conv. kernel depth
+  K4 = 20    # forth conv. kernel depth
+  F1 = 500   # first FC layer node size
+  F2 = 50    # second FC layer node size
 
-W4 = tf.Variable(tf.truncated_normal([25 * 25 * M, N], stddev=0.1), name='W4')
-B4 = tf.Variable(tf.constant(0.1, tf.float32, [N]), name='B4')
-W5 = tf.Variable(tf.truncated_normal([N, 4], stddev=0.1), name='W5')
-B5 = tf.Variable(tf.constant(0.1, tf.float32, [4]), name='B5')
+  with tf.variable_scope('Conv1'):
+    y1 = ConvLayer(X, 3, K1, 100, 10).out()
+  with tf.variable_scope('BN'):
+    y1 = BatchNorm(y1, K1, train).out()
+  with tf.variable_scope('Conv2'):
+    y2 = ConvLayer(y1, K1, K2, 10, 2).out()
+  with tf.variable_scope('Conv3'):
+    y3 = ConvLayer(y2, K2, K3, 5, 2).out()
+  with tf.variable_scope('Conv4'):
+    y4 = ConvLayer(y3, K3, K4, 3, 1).out()
+  y4_rs = tf.reshape(y4, shape=[-1, 25*25*K4])
 
-stride = 10
-Y1 = tf.nn.relu(tf.nn.conv2d(X, W1, strides=[1, stride, stride, 1], padding='SAME') + B1)
-Y1 = batch_norm(Y1, K, phase_train)
-stride = 2
-Y2 = tf.nn.relu(tf.nn.conv2d(Y1, W2, strides=[1, stride, stride, 1], padding='SAME') + B2)
-#Y2 = batch_norm(Y2, L, phase_train)
-stride = 2
-Y3 = tf.nn.relu(tf.nn.conv2d(Y2, W3, strides=[1, stride, stride, 1], padding='SAME') + B3)
-#Y3 = batch_norm(Y3, M, phase_train)
-YY = tf.reshape(Y3, shape=[-1, 25 * 25 * M])
+  with tf.variable_scope('Full1'):
+    y5 = FCLayer(y4_rs, 25*25*K4, F1, relu=True).out()
+    y5_do = tf.cond(train, lambda:tf.nn.dropout(y5, p_keep), lambda:y5)
+  with tf.variable_scope('Full2'):
+    y6 = FCLayer(y5_do, F1, F2, relu=True).out()
+    y6_do = tf.cond(train, lambda:tf.nn.dropout(y6, p_keep), lambda:y6)
+  with tf.variable_scope('Output'):
+    Ylogits = FCLayer(y6_do, F2, 4).out()
+  Y = tf.nn.softmax(Ylogits, name='Y')
 
-Y4 = tf.nn.relu(tf.matmul(YY, W4) + B4)
-YY4 = tf.nn.dropout(Y4, p_keep)
-Ylogits = tf.matmul(YY4, W5) + B5
-Y = tf.nn.softmax(Ylogits, name='Y')
+  with tf.variable_scope('cross_entropy'):
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
+    cross_entropy = tf.reduce_mean(cross_entropy)
+  with tf.variable_scope('accuracy'):
+    correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    incorrects = tf.squeeze(tf.where(tf.logical_not(correct_prediction)), [1])
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
-cross_entropy = tf.reduce_mean(cross_entropy)
-tf.summary.scalar('cross_entropy', cross_entropy)
+  return Y, cross_entropy, accuracy, incorrects
 
-correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.summary.scalar('accuracy', accuracy)
+if __name__ == '__main__':
+  train = True
 
-train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy, global_step=global_step)
+  if train:
+    TOTAL_STEP = 3000
+    BATCH_SIZE = 120
+    INPUT_PATH = '/mnt/data/camus/images/20170919/'
+    MODEL_PATH = './tmp/train/model/cnn.ckpt'
+    LOG_DIR = './tmp/train/log/'
 
-# read input png
-files = os.listdir(INPUT_DIR)
-files = [os.path.join(INPUT_DIR,s) for s in files]
+    startTime = time.time()
+    # input generation
+    with tf.Graph().as_default() as input_g:
+      XY_s = DataGen(INPUT_PATH, BATCH_SIZE).out()
+    print('[%6.2f] successfully generated train data'%(time.time()-startTime))
 
-file_queue = tf.train.string_input_producer(files, shuffle=False)
-image_reader = tf.WholeFileReader()
+    # training phase
+    with tf.Graph().as_default() as train_g:
+      # input X: 1000 x 1000 rgb color image
+      X = tf.placeholder(tf.float32, [None, 1000, 1000, 3], name='X')
+      # target values Y_: 0=normal, 1=stator fault, 2=rotor fault, 3=bearing fault
+      Y_ = tf.placeholder(tf.float32, [None, 4], name='Y_')
+      with tf.variable_scope('Config'):
+        # dropout keep probability
+        p_keep = tf.placeholder(tf.float32, name='p_keep')
+        train = tf.placeholder(tf.bool, name='train')
+        # learning rate
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        lr = tf.train.exponential_decay(0.001, global_step, int(TOTAL_STEP/5), 0.5, staircase=True, name='lr')
 
-img_path, img_bin = image_reader.read(file_queue)
-image = tf.image.decode_png(img_bin, channels=3)
+      # load inference model
+      Y, cross_entropy, accuracy, _ = model(X, Y_, train)
+      train_op = tf.train.AdamOptimizer(lr).minimize(cross_entropy, global_step=global_step)
 
-# set tensorboard summary and saver
-merged = tf.summary.merge_all()
-saver = tf.train.Saver()
+      with tf.variable_scope('Metrics'):
+        tf.summary.scalar('accuracy', accuracy)
+        tf.summary.scalar('cross_entropy', cross_entropy)
+        tf.summary.scalar('learning_rate', lr)
 
-# start session
-startTime = time.time()
-with tf.Session() as sess:
-  sum_writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
+      # set tensorboard summary and saver
+      merged = tf.summary.merge_all()
+      saver = tf.train.Saver()
 
-  tf.global_variables_initializer().run()
+      # training session
+      print('----- training start -----')
+      with tf.Session() as sess:
+        sum_writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
+        tf.global_variables_initializer().run()
 
-  print('----- training start -----')
-  coord = tf.train.Coordinator()
-  threads = tf.train.start_queue_runners(coord=coord)
+        for step in range(int(TOTAL_STEP/len(XY_s))):
+          i = 0
+          for X_batch, Y_batch in XY_s:
+            _, summary, acc, ent = sess.run([train_op, merged, accuracy, cross_entropy],
+                                           {X:X_batch, Y_:Y_batch, p_keep:0.75, train:True})
+            sum_writer.add_summary(summary, step*len(XY_s)+i+1)
+            print('[%6.2f] step:%3d, lr:%f, accuracy:%f, cross entropy:%f'
+                  %(time.time()-startTime, step*len(XY_s)+i+1, lr.eval(), acc, ent))
+            i += 1
+          saver.save(sess, MODEL_PATH, global_step = step)
+        print('-----  training end  -----')
 
-  X_batch = []
-  Y_batch = []
-  for i in range(len(files)):
-    # image shape = 1000 x 1000 x 3 = [1000, 1000, 3]
-    X_i, Y_i = sess.run([image, img_path])
-    X_batch.append(X_i)
-    Y_batch.append(int(Y_i.strip('.png').split('-')[3]))
+  else:
+    INPUT_PATH = '/mnt/data/camus/images/20170830/'
+    MODEL_PATH = './tmp/test/model/cnn.ckpt'
+    LOG_DIR = './tmp/test/log/'
 
-  coord.request_stop()
-  coord.join(threads)
+    startTime = time.time()
+    # input generation
+    with tf.Graph().as_default() as input_g:
+      XY_s = DataGen(INPUT_PATH, train=False).out()
+    print('[%6.2f] successfully generated test data'%(time.time()-startTime))
 
-  xys = []
-  for x, y in chunks((X_batch,Y_batch), 50):
-    xys.append((x,y))
- 
-  print('[%6.2f] input generated'%(time.time()-startTime))
+    # test phase
+    with tf.Graph().as_default() as test_g:
+      # input X: 1000 x 1000 rgb color image
+      X = tf.placeholder(tf.float32, [None, 1000, 1000, 3], name='X')
+      # target values Y_: 0=normal, 1=stator fault, 2=rotor fault, 3=bearing fault
+      Y_ = tf.placeholder(tf.float32, [None, 4], name='Y_')
+      with tf.variable_scope('Config'):
+        # dropout keep probability
+        p_keep = tf.placeholder(tf.float32, name='p_keep')
+        train = tf.placeholder(tf.bool, name='train')
 
-  for step in range(int(TOTAL_STEP/len(xys))):
-    i = 0
-    for x_chunk, y_chunk in xys:
-      y_chunk = tf.one_hot(y_chunk,4).eval()
-      _, summary, acc, ent, y_predict = sess.run([train_step, merged, accuracy, cross_entropy, Y],
-                                     {X:x_chunk, Y_:y_chunk, p_keep:0.75, phase_train:True})
-      sum_writer.add_summary(summary, step*len(xys)+i+1)
-      print('[%6.2f] step:%3d, lr:%f, accuracy:%f, entropy:%f'
-            %(time.time()-startTime, step*len(xys)+i+1, lr.eval(), acc, ent))
-#      print(y_predict) #PROTO
-      i += 1
-    saver.save(sess, MODEL_DIR+'cnn.ckpt', global_step = step)
-  print('----- training end -----')
+      # load inference model
+      Y, cross_entropy, accuracy, incorrects = model(X, Y_, train)
+
+      with tf.variable_scope('Metrics'):
+        tf.summary.scalar('accuracy', accuracy)
+        tf.summary.scalar('cross_entropy', cross_entropy)
+
+      # set tensorboard summary and saver
+      merged = tf.summary.merge_all()
+      saver = tf.train.Saver()
+
+      # test session
+      print('----- test start -----')
+      with tf.Session() as sess:
+        sum_writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
+        tf.global_variables_initializer().run()
+        saver.restore(sess, MODEL_PATH)
+        summary, acc, ent, incor, y, y_ = sess.run([merged, accuracy, cross_entropy, incorrects, Y, Y_],
+                                                    {X:XY_s[0], Y_:XY_s[1], p_keep:0.00, train:False})
+        sum_writer.add_summary(summary)
+        print('[%6.2f] average accuracy:%f, cross entropy:%f'%(time.time()-startTime, acc, ent))
+        print('   incorrects list:')
+        for i in incor:
+          print('   [%3d] Answer:Infer = %d:%d  at %s'
+                  %(i, tf.argmax(y_[i],0).eval(),tf.argmax(y[i],0).eval(),XY_s[2][i]))
+        print('-----  test end  -----')
